050c13b1e209780445c5292338442701ca1032a2
EC2 Default User <ec2-user@ip-172-31-12-219.us-east-2.compute.internal>
2023-07-18 15:40:33 +0000

    課題提出

---
+++ main.js
@@ -0,0 +1,48 @@
+const time = document.getElementById("time");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+
+
+let startTime = 0;
+let stopTime = 0;
+let elapsedTime = 0;
+let currentTime = "";
+let timeoutID;
+
+function displayTime(){
+    const currentTime = new Date(Date.now() - startTime + stopTime);
+    const h = String(currentTime.getHours() - 9);
+    const m = String(currentTime.getMinutes());
+    const s = String(currentTime.getSeconds());
+    const ms = String(currentTime.getMilliseconds()).slice(0,1);
+    
+    time.textContent = `${h}:${m}:${s}:${ms}`;
+    timeoutID = setTimeout(displayTime,10);
+}
+
+startButton.addEventListener('click',function(){
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+    startTime = Date.now();
+    stopTime = 0;
+    displayTime();
+});
+
+stopButton.addEventListener('click',function(){
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+    clearTimeout(timeoutID);
+    stopTime += (Date.now() - startTime);
+});
+
+resetButton.addEventListener('click',function() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = true;
+    time.textContent = '0:0:0:0';
+    stopTime = 0;
+});
